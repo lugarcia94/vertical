e703d6d3dddcde87d1a20b362807a24693eb108b
Lucas Costa <lucas.garcia@auaha.com.br>
2020-01-03 15:24:31 -0300

    merge

--- src/footer.php
+++ src/footer.php
@@ -20,7 +20,7 @@
                 </div>
             </div>
         </div>
-        <span>Lemon Auaha © 2020 - Todos os direitos reservados</span>
+        <span>Paper Auaha © 2020 - Todos os direitos reservados</span>
     </footer>
 	</div><!-- #content -->
 
